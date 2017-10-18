021eb237565876ad91324f71082f49d5892ca906
SeanCarroll17 <31080468+SeanCarroll17@users.noreply.github.com>
2017-10-18 19:46:59 +0100

    Update GIFsearch.js
    removed some comments

--- GIFsearch.js
+++ GIFsearch.js
@@ -6,7 +6,7 @@ function searchForm() {
 	var xhr = $.get("http://api.giphy.com/v1/gifs/search?q=" + search + "&api_key=A3H0p6eePowjUTlOohUnEWnyDBLPkAH6");
 	xhr.done(function(data) {
 		var $image = data; 
-		var $GIF = $image['data'][0]['images']['fixed_width']['url']//['images']['fixed_width']['mp4']
+		var $GIF = $image['data'][0]['images']['fixed_width']['url']
 
 		var GIF_image = document.createElement('img');
 		GIF_image.setAttribute('src', $GIF);
